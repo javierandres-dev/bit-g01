3ed7511914d5c61ff103de7c6c38fa3b7d781107
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-04-21 21:13:19 -0500

    api restful

---
+++ 14/README.md
@@ -0,0 +1,9 @@
+# :octocat: bit-course-full_stack_01/14
+- - -
+## Clase 2022/04/21
+### Términos y conceptos
+### Recursos
+### Ejercicios propuestos para prácticar
+- - -
+## Software Developer
+[Javier Andrés Garzón Patarroyo](https://javierandresgp.com)
---
+++ 14/backend/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const todosRouter = require('./routes/todos');
+
+const server = express();
+const port = 4000;
+
+server.use(express.json());
+server.use('/api/v1', todosRouter);
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch((err) =>
+    console.log('Error al conectar a la base de datos, error: ' + err)
+  );
+
+server.get('/', (request, response) => {
+  response.send('Hola desde la raíz');
+});
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en localhost, en el puerto ${port}`);
+});
