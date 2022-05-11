0e41d4a3e83bb6eb8a36e6a236a6104f462137c8
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-05-10 21:57:25 -0500

    angular spa

---
+++ 19/README.md
@@ -0,0 +1,39 @@
+# :octocat: bit-course-full_stack_01/01
+- - -
+## Clase 2022/02/28
+### Términos y conceptos
+* [x] Importancia del idioma inglés
+* [x] Internet
+* [x] Web
+* [x] Página web
+* [x] Sitio web
+* [x] Aplicación web
+* [x] Desarrollo web
+* [x] Servidor
+* [x] Cliente
+* [x] Full stack
+* [x] Base de datos
+* [x] Back-End
+* [x] Front-End
+* [x] Interface de comandos en línea
+* [x] Editor de texto
+* [x] Navegador de internet
+* [x] Sistema de control de versiones
+* [x] Hosting de control de versiones
+* [x] Repositorio
+* [x] Importancia del archivo README.md en un repositorio
+### Ejercicios propuestos para prácticar
+* Creación de cuenta en GitHub
+* Crear repositorio en GitHub: "bit-iberoamericana-desarrollo-web-full-stack"
+* Instalación de Visual Studio Code
+* Instalación de Git
+* Clonar repositorio: "bit-iberoamericana-desarrollo-web-full-stack"
+* Comandos y uso básico de git
+* Prueba de sincronia entre Git - GitHub
+### Recursos
+* [Git documentation](https://git-scm.com/doc)
+* [GitHub documentation](https://docs.github.com/)
+* [Visual Studio Code documentation](https://code.visualstudio.com/docs)
+- - -
+## Software Developer
+[Javier Andrés Garzón Patarroyo](https://javierandresgp.com)
---
+++ 19/frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PrivateComponent } from './components/private/private.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PrivateComponent,
+    RegisterComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'private', component: PrivateComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
